5b483615a588f59e5d6edfb4c90bbabd5886fbad
sebastian-andreas.si <sebastian-andreas.si@MI000-GS036-10.mathinfo.i-univ-tlse2.fr>
2017-12-12 12:14:17 +0100

    fixed JaugeNaturel once more

--- src/JaugeNaturel/JaugeTest.java
+++ src/JaugeNaturel/JaugeTest.java
@@ -4,9 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import JaugeNaturel.Jauge.TypeJauge;
 
-public class JaugeTest implements IJaugeTest{
+public class JaugeTest{
 	public enum TypeJauge {
 		JaugeDistance, 
 		JaugeNaturel,
